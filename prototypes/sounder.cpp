5000cbe1e10e79aad4a2205491958a4944f2b3a1
Jimi Wills <jimiwills@gmail.com>
2022-10-23 17:16:17 +0100

    notes :)

--- prototypes/sounder.cpp
+++ prototypes/sounder.cpp
@@ -7,21 +7,26 @@
 class Generator {
 	public:
 	int rate;
-	int generate(float length, int freq, unsigned char* buffer){
+	int generate(float length, int freq, unsigned char* buffer, int start){
 		int W = rate/(float)freq;
 		int L = length * (float) rate;
+		
+		// TODO: force a whole number of waves into the time
+		
+		// ALSO TODO: copy the maths from buidling-music-logic/* into here
+		
 		for(int i=0; i<L; i++){
-			buffer[i] = (unsigned char) ((sin(2*PI*freq* i/rate)+1)/2*255.0);
+			buffer[start+i] = (unsigned char) ((sin(2*PI*freq* i/rate)+1)/2*255.0);
 		}
-		return L;
+		return start+L;
 	}
 };
 
 int addSemitone(int note){
-	return (int) pow(2, 1/12) * float(note);
+	return (int) (pow(2.0, 1.0/12.0) * float(note));
 }
 int addWholetone(int note){
-	return (int) pow(2, 1/6) * float(note);
+	return (int) (pow(2.0, 1.0/6.0) * float(note));
 }
 
 int main(int argc, char *argv[])
@@ -31,27 +36,36 @@ int main(int argc, char *argv[])
 	int C = addSemitone(B);
 	int D = addWholetone(C);
 	int E = addWholetone(D);
-	int F = addWholetone(E);
+	int F = addSemitone(E);
 	int G = addWholetone(F);
 	int a = addWholetone(G);
-	
+	int b = addWholetone(a);
+
 	int rate;
 	std::stringstream(argv[1]) >> rate;
 	
 	Generator gen;
 	gen.rate = rate;
 	
-	unsigned char buffer[78000];
+	unsigned char buffer[2*1024*1024];
 	
-	int l = gen.generate(0.4, A, buffer);
+	int l = 0;
+	l = gen.generate(0.4, A, buffer, l);
+	l = gen.generate(0.4, C, buffer, l);
+	l = gen.generate(0.4, B, buffer, l);
+	l = gen.generate(0.4, D, buffer, l);
+	l = gen.generate(0.4, C, buffer, l);
+	l = gen.generate(0.4, E, buffer, l);
+	l = gen.generate(0.4, D, buffer, l);
+	l = gen.generate(0.4, F, buffer, l);
+	l = gen.generate(0.4, E, buffer, l);
+	l = gen.generate(0.4, G, buffer, l);
+	l = gen.generate(0.4, F, buffer, l);
+	l = gen.generate(0.4, a, buffer, l);
+	l = gen.generate(0.4, G, buffer, l);
+	l = gen.generate(0.4, b, buffer, l);
+	l = gen.generate(0.8, a, buffer, l);
 	fwrite(buffer, 1, l, stdout);
-	/*std::cout << gen.generate(0.4, B);
-	std::cout << gen.generate(0.4, C);
-	std::cout << gen.generate(0.4, D);
-	std::cout << gen.generate(0.4, E);
-	std::cout << gen.generate(0.4, F);
-	std::cout << gen.generate(0.4, G);
-	std::cout << gen.generate(1, a);
-	*/
+	
 	return 0;
 }
